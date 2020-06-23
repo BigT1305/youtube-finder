8d9fef8ca5d4494ba4508809a6db427261569b5c
BigT1305 <anthony.hoss@selu.edu>
2020-06-22 21:49:27 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './Components/searchBar';
+import VideoList from './Components/videoList';
+import VideoDetail from './Components/videoDetail';
+import key from './key';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API = {
+  key: key.API_KEY,
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectVideo: null
+    };
+
+    this.videoSearch('CrossFit Workout');
+  }
+
+  videoSearch(searchTerm) {
+    YTSearch({ key: key.API_KEY, term: searchTerm}, (data) => {
+      console.log(data);
+      this.setState({
+        videos: data,
+        selectedVideo: data[0]
+      });
+    });
+  }
+  
+  render() {
+    return (
+      <div>
+        <SearchBar onSearchTermChange={searchTerm => this.videoSearch(searchTerm)}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList 
+          onVideoSelect={userSelected => this.setState({selectedVideo: userSelected})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/key.js
@@ -0,0 +1,3 @@
+module.exports = {
+	API_KEY: 'AIzaSyASk6rA4fPkBQdbqlllFASlXQR3W5Fmcs8',
+};
